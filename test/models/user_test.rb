0f16025ff552597c99de4c0a05d15f7120d11b87
Jeremy DeCarvalho <jeremypdecarvalho@gmail.com>
2018-10-14 20:29:08 -0700

    set up backend, create first version of the form in html, create index.html.erb to show users in the db

---
+++ test/models/user_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+  	@user = User.new(username: "The Guy", email: "theguy@theguy.com", password: "secretGuy123")
+  end
+  test "should be valid" do 
+  	assert @user.valid?
+  end
+
+  test "username should be present" do 
+  	@user.username = "         "
+  	assert_not @user.valid?
+  end    
+
+  test "email should be present" do 
+  	@user.email = "         "
+  	assert_not @user.valid?
+  end    
+
+  test "password should be present" do 
+  	@user.password = "         "
+  	assert_not @user.valid?
+  end  
+end
